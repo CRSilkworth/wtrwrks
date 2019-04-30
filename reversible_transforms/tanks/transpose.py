fa87a10efe4232e3d0ebb0dc441e7c5c74c56ee9
CRSilkworth <CRSilkworth@gmail.com>
2019-05-01 07:17:50 +0900

    added transpose

---
+++ reversible_transforms/tanks/transpose.py
@@ -0,0 +1,109 @@
+import reversible_transforms.waterworks.waterwork_part as wp
+import reversible_transforms.waterworks.tank as ta
+import reversible_transforms.tanks.utils as ut
+import numpy as np
+
+
+def transpose(a, axes, type_dict=None, waterwork=None, name=None):
+  """Find the min of a np.array along one or more axes in a reversible manner.
+
+  Parameters
+  ----------
+  a : Tube, np.ndarray or None
+      The array to get the min of.
+  axes : Tube, int, tuple or None
+      The axes (axes) along which to take the min.
+  type_dict : dict({
+    keys - ['a', 'b']
+    values - type of argument 'a' type of argument 'b'.
+  })
+    The types of data which will be passed to each argument. Needed when the types of the inputs cannot be infered from the arguments a and b (e.g. when they are None).
+
+  waterwork : Waterwork or None
+    The waterwork to add the tank (operation) to. Default's to the _default_waterwork.
+  name : str or None
+      The name of the tank (operation) within the waterwork
+
+  Returns
+  -------
+  Tank
+      The created add tank (operation) object.
+
+  """
+  type_dict = ut.infer_types(type_dict, a=a, axes=axes)
+
+  return Transpose(a=a, axes=axes, waterwork=waterwork, name=name)
+
+
+class Transpose(ta.Tank):
+  """The min class. Handles 'a's of np.ndarray type.
+
+  Attributes
+  ----------
+  slot_keys : list of str
+    The tank's (operation's) argument keys. They define the names of the inputs to the tank.
+  tube_dict : dict(
+    keys - strs. The tank's (operation's) output keys. THey define the names of the outputs of the tank
+    values - types. The types of the arguments outputs.
+  )
+    The tank's (operation's) output keys and their corresponding types.
+
+  """
+
+  slot_keys = ['a', 'axes']
+  tube_dict = {
+    'target': np.ndarray,
+    'axes': tuple
+  }
+
+  def _pour(self, a, axes):
+    """Execute the add in the pour (forward) direction .
+
+    Parameters
+    ----------
+    a : np.ndarray
+      The array to take the min over.
+    axes : int, tuple
+      The axes (axes) to take the min over.
+
+    Returns
+    -------
+    dict(
+      'target': np.ndarray
+        The result of the min operation.
+      'a': np.ndarray
+        The original a
+      'axes': in, tuple
+        The axes (axes) to take the min over.
+    )
+
+    """
+    # Must just return 'a' as well since so much information is lost in a
+    # min
+    return {'target': np.transpose(a, axes=axes), 'axes': axes}
+
+  def _pump(self, target, axes):
+    """Execute the add in the pump (backward) direction .
+
+    Parameters
+    ----------
+    target: np.ndarray
+      The result of the min operation.
+    a : np.ndarray
+      The array to take the min over.
+    axes : int, tuple
+      The axes (axes) to take the min over.
+
+    Returns
+    -------
+    dict(
+      'a': np.ndarray
+        The original a
+      'axes': in, tuple
+        The axes (axes) to take the min over.
+    )
+
+    """
+    trans_axes = np.argsort(axes)
+    a = np.transpose(target, axes=trans_axes)
+    return {'a': a, 'axes': axes}
