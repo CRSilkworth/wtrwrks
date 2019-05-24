b1581a2d646c13f36debedcdd91953b4693cfa44
CRSilkworth <CRSilkworth@gmail.com>
2019-05-24 21:04:01 +0900

    doc strings written

--- waterworks/string_manipulations/diff.py
+++ waterworks/string_manipulations/diff.py
@@ -1,10 +1,30 @@
+"""Functions which deal with differences between strings."""
 import difflib
 import json
 
 
 def get_diff_string(source_string, target_string):
+  """Generate a string which can be used to transform one string into another.
+
+  Parameters
+  ----------
+  source_string : str
+    The string that uses the diff_string to get transformed into the target string.
+  target_string : type
+    The string that source string is being transformed into.
+
+  Returns
+  -------
+  str
+    A string that describes a sequence of operations on source_string to produce target_string.
+
+  """
+  # Split up the strings into lists.
   source_string = list(source_string)
   target_string = list(target_string)
+
+  # Use a sequence matcher to identify all the common substrings of
+  # source_string an target_string.
   matcher = difflib.SequenceMatcher(None, source_string, target_string)
 
   tag_map = {
@@ -12,6 +32,9 @@ def get_diff_string(source_string, target_string):
     'insert': 'i',
     'replace': 'i'
   }
+
+  # Get out the operation codes which list out all the operations that need to
+  # occur in order to transform source_string into target_string. 
   diff_string = []
   for tag, i1, i2, j1, j2 in matcher.get_opcodes():
     if tag == 'equal':
@@ -27,6 +50,21 @@ def get_diff_string(source_string, target_string):
 
 
 def reconstruct(source_string, diff_string):
+  """Generate a string which can be used to transform one string into another.
+
+  Parameters
+  ----------
+  source_string : str
+    The string that uses the diff_string to get transformed into the target string.
+  target_string : str
+    A string that describes a sequence of operations on source_string to produce target_string.
+
+  Returns
+  -------
+  str
+    The string that source string is transformed into using the diff string.
+
+  """
   target_string = list(source_string)
   for tag, i1, i2, substring in reversed(json.loads(diff_string)):
     if tag == 'd':
